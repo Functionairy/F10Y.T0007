d7a08e707f1a3357ec09f838e64e56782fbb95a5
SURFEIT\David <david.coats@gmail.com>
2025-03-23 11:46:54 -0700

    Add entity marker attribute type.

---
+++ source/F10Y.T0007/Code/Attributes/EntityMarkerAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using F10Y.T0000;
+
+
+namespace F10Y.T0007
+{
+    /// <summary>
+    /// Attribute indicating a class is an entity (a class specialized to participate in data storage with Entity Framework Core).
+    /// </summary>
+    /// <remarks>
+    /// The marker attribute is useful for surveying entity classes and building a catalogue of entities.
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Class, // Scripts are the same as functions (just no input or output arguments).
+        AllowMultiple = false, // No.
+        Inherited = false // Make inheritors all use the marker type.
+    )]
+    [MarkerAttributeMarker]
+    public class EntityMarkerAttribute : Attribute
+    {
+        /// <summary>
+        /// Allows specifying that a class is *not* an entity class.
+        /// This is useful for marking classes that end up canonical entity code file locations, but are not entity classes.
+        /// </summary>
+        public bool Is_Entity { get; }
+
+
+        public EntityMarkerAttribute(
+            bool is_Entity = true)
+        {
+            this.Is_Entity = is_Entity;
+        }
+    }
+}
